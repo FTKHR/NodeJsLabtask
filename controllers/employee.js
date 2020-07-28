3f52c9651008feb655f6e5411b1b739d156bca56
Iftekhar Alam Khan <iftysoftwares02@gmail.com>
2020-07-28 23:11:57 +0600

    T2M3F2

--- controllers/employee.js
+++ controllers/employee.js
@@ -5,4 +5,24 @@ router.get('/', function(req, res){
 	res.render('employee/index');
 });
 
+router.post('/', function(req, res){
+
+	if(req.body.choice=="MyProfile"){
+		res.redirect('/employee/profile');
+    }
+    /*else if(req.body.choice=="UpdateProfile"){
+		res.redirect('/employee/profile');
+    }*/
+	
+});
+router.get('/profile',function(req,res){
+    var data={
+        name:'Iftekhar',
+        phone:'012345678910',
+        designation:'Engineer'
+    }
+    res.render('employee/profile',data);
+});
+
+
 module.exports = router;
