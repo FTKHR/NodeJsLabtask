71ffbdf35e4333567da7182b5050d7b4b8ce335e
Iftekhar Alam Khan <iftysoftwares02@gmail.com>
2020-07-29 17:17:22 +0600

    T2M2F2

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,5 +4,13 @@ var router = express.Router();
 router.get('/', function(req, res){
 	res.render('admin/index');
 });
+router.post('/',function(req,res) {
+	if(req.body.choice=="Add Employee"){
+		res.redirect('/admin/addEmployee')
+	}
+})
 
+router.get('/addEmployee', function (req, res) {
+	res.render('admin/addEmployee');
+})
 module.exports = router;
